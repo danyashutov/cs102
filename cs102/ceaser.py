83b63f42d77b760665cfde6280fbeec6b669a4f3
Daniil19112001 <Daniil19112001>
2019-10-11 13:16:21 +0300

    new file

--- cs102/ceaser.py
+++ cs102/ceaser.py
@@ -1,1 +1,33 @@
-ruslan bog
+def encrypt_caesar(plaintext: str) -> str:
+    for i in plaintext:
+        x = ord(i)
+        if (x > 64 and x < 123):
+            if (x > 87 and x < 91) or (x < 123 and x > 119):
+                x = x - 26
+            y = chr(x + 3)
+            plaintext = plaintext.replace(i, y)
+    print(plaintext)
+    return plaintext
+
+
+encrypt_caesar("PYTHON")
+encrypt_caesar("python")
+encrypt_caesar("Python3.6")
+
+
+def decrypt_caesar(ciphertext: str) -> str:
+    for i in ciphertext:
+
+        x = ord(i)
+        if (x > 64 and x < 123):
+            if (x < 68 and x < 91) or (x < 100 and x > 96):
+                x = x + 26
+            y = chr(x - 3)
+            ciphertext = ciphertext.replace(i, y)
+        print(ciphertext)
+        return ciphertext
+
+
+decrypt_caesar("SBWKRQ")
+decrypt_caesar("sbwkrq")
+decrypt_caesar("Sbwkrq3.6")
