cd4027ba329a96d1dd20681d671ecf384a799f9f
Daniil19112001 <Daniil19112001>
2019-10-12 14:31:14 +0300

    new file

--- cs102/vigenere.py
+++ cs102/vigenere.py
@@ -12,22 +12,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     # PUT YOUR CODE HERE
     ciphertext = ''
     i = 0;
-    for ch in plaintext:
+    for k in plaintext:
 
-        if 'a' <= ch <= 'z' or 'A' <= ch <= 'Z':
+        if 'a' <= k <= 'z' or 'A' <= k <= 'Z':
             shift = ord(keyword[i % len(keyword)])
             if 'a' <= chr(shift) <= 'z':
                 shift -= ord('a')
             else:
                 shift -= ord('A')
-            code = ord(ch) + shift
-            if 'a' <= ch <= 'z' and code > ord('z'):
+            code = ord(k) + shift
+            if 'a' <= k <= 'z' and code > ord('z'):
                 code -= 26
-            elif 'A' <= ch <= 'Z' and code > ord('Z'):
+            elif 'A' <= k <= 'Z' and code > ord('Z'):
                 code -= 26
             ciphertext += chr(code)
         else:
-            ciphertext += ch
+            ciphertext += k
         i = 1 + i
     return ciphertext
 
@@ -45,21 +45,21 @@ def decrypt_vigenere(ciphertext, keyword):
     # PUT YOUR CODE HERE
     plaintext = ''
     i = 0;
-    for ch in ciphertext:
-        if 'a' <= ch <= 'z' or 'A' <= ch <= 'Z':
+    for k in ciphertext:
+        if 'a' <= k <= 'z' or 'A' <= k <= 'Z':
             shift = ord(keyword[i % len(keyword)])
             if 'a' <= chr(shift) <= 'z':
                 shift -= ord('a')
             else:
                 shift -= ord('A')
-            code = ord(ch) - shift
-            if 'a' <= ch <= 'z' and code < ord('a'):
+            code = ord(k) - shift
+            if 'a' <= k <= 'z' and code < ord('a'):
                 code += 26
-            elif 'A' <= ch <= 'Z' and code < ord('A'):
+            elif 'A' <= k <= 'Z' and code < ord('A'):
                 code += 26
             plaintext += chr(code)
         else:
-            plaintext += ch
+            plaintext += k
         i = 1 + i
     return plaintext
 
