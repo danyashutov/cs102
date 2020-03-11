2453f39f1e2ac4703eec639b30247f1f9c1dd14d
Daniil19112001 <danya.shutov@inbox.ru>
2020-03-11 22:58:52 +0300

    Homework06 template

--- cs102/homework06/hackernews.py
+++ cs102/homework06/hackernews.py
@@ -5,8 +5,8 @@ from sqlalchemy.testing import db
 from cs102.homework06.bayes import NaiveBayesClassifier
 from cs102.homework06.scraputils import get_news
 from cs102.homework06.db import News, session
-
-
+import bottle
+@bottle.route('/')
 @route("/news")
 def news_list():
     s = session()
@@ -16,13 +16,32 @@ def news_list():
 
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
+    s = session()
+    id = request.query.id
+    label = request.query.label
+    news = s.query(News).get(id)
+    news.label = label
+    s.commit()
     redirect("/news")
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    s = session()
+    base = s.query(News).all()
+    new_news = get_news('https://news.ycombinator.com/newest', 34)
+    for i in new_news:
+        for j in base:
+            if (i['title'] == j.title) and (i['author'] == j.author):
+                break
+            else:
+                news = News(title=i['title'],
+                            author=i['author'],
+                            url=i["url"],
+                            comments=i['comments'],
+                            points=i['points'])
+                s.add(news)
+                s.commit()
     redirect("/news")
 
 
@@ -31,6 +50,6 @@ def classify_news():
     # PUT YOUR CODE HERE
     redirect("/news")
 
+
 if __name__ == "__main__":
     run(host="localhost", port=8080)
-
